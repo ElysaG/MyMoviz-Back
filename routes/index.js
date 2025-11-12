6d49302bb8a002db21f2a31201ad8faffc19abde
Elisa Gomez <elysa3148@gmail.com>
2025-11-12 12:00:00 +0100

    Ajout async await et modifications tableau

--- routes/index.js
+++ routes/index.js
@@ -4,25 +4,30 @@ const fetch = require("node-fetch");
 
 const API_KEY = process.env.API_KEY;
 
-router.get("/movies", (req, res) => {
-  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
-    .then((res) => res.json())
-    .then((apiData) => {
-      if (apiData) {
-        // Version initiale:
-        // return res.json({ movies: apiData });
-        const movies = apiData.results.map((movie) => ({
-          title: movie.title,
-          poster_path: movie.poster_path,
-          overview: movie.overview,
-          vote_average: movie.vote_average,
-          vote_count: movie.vote_count,
-        }));
-        res.json({ movies }); //Renvoie un objet {movies:[]} comme dans la consigne!
-      } else {
-        return res.json({ result: false, error: "not found" });
-      }
-    });
+router.get("/movies", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
+    );
+    const apiData = await response.json();
+
+    if (!apiData.results) {
+      return res.json({ result: false, error: "pas de résultat API fetch" });
+    }
+
+    const movies = apiData.results.map((movie) => ({
+      title: movie.title,
+      poster_path: movie.poster_path,
+      overview: movie.overview,
+      vote_average: movie.vote_average,
+      vote_count: movie.vote_count,
+    }));
+
+    res.json({ movies }); //Renvoie un objet {movies:[]} comme dans la consigne!
+  } catch (err) {
+    console.error(err);
+    return res.json({ result: false, error: "erreur au catch" });
+  }
 });
 
 module.exports = router;
